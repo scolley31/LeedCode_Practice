9bd45d8bcfc5431152e66c50c573a96c5a658d49
scolley <scolley.huang@kkday.com>
2023-07-09 06:41:46 +0800

    20. Valid Parentheses

---
+++ 20. Valid Parentheses/ValidParentheses.java
@@ -0,0 +1,37 @@
+package org.example;
+
+import java.util.Stack;
+
+public class ValidParentheses {
+    class Solution {
+        public static boolean isValid(String s) {
+            Stack<Character> isValidStack = new Stack<>() ;
+            char[] chars = s.toCharArray();
+            for (char aChar : chars) {
+
+                switch (aChar) {
+                    case '(', '[', '{' -> isValidStack.push(aChar);
+                    case ')' -> {
+                        if (isValidStack.isEmpty()) return false;
+                        if (isValidStack.pop() != '(') {
+                            return false;
+                        }
+                    }
+                    case ']' -> {
+                        if (isValidStack.isEmpty()) return false;
+                        if (isValidStack.pop() != '[') {
+                            return false;
+                        }
+                    }
+                    case '}' -> {
+                        if (isValidStack.isEmpty()) return false;
+                        if (isValidStack.pop() != '{') {
+                            return false;
+                        }
+                    }
+                }
+            }
+            return  isValidStack.isEmpty();
+        }
+    }
+}
