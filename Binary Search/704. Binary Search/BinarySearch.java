61e7c573d1c8fde17bc863ae1adc2c045b02269c
scolley <scolley.huang@kkday.com>
2023-07-17 10:30:50 +0800

    704. Binary Search

---
+++ Binary Search/704. Binary Search/BinarySearch.java
@@ -0,0 +1,27 @@
+package org.example;
+
+
+/*
+ * Time Complexity O(log(n))
+ * Space Complexity O(1)
+ * */
+
+public class BinarySearch {
+    class Solution {
+        public int search(int[] nums, int target) {
+            int left = 0;
+            int right = nums.length - 1;
+            while (left <= right) {
+                int mid = left + (right - left)/2;
+                if (nums[mid] > target) {
+                    right = mid - 1;
+                } else if (nums[mid] < target) {
+                    left = mid + 1;
+                } else {
+                    return  mid;
+                }
+            }
+            return -1;
+        }
+    }
+}
