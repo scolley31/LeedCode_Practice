9cb10c5e4316723a0290264a24422c9c4cedd186
scolley <scolley.huang@kkday.com>
2023-08-16 07:48:10 +0800

    75. Sort Colors - counting sort

--- Sorting/75. Sort Colors/SortColors.java
+++ Sorting/75. Sort Colors/SortColors.java
@@ -1,7 +1,5 @@
 package org.example;
 
-import java.util.Arrays;
-
 public class SortColors {
 
     /*
@@ -9,12 +7,14 @@ public class SortColors {
      * Space Complexity O(1)
      *
      * Dutch National Flag Problem
+     *
+     * one pass
      * */
 
-    public void sortColors(int[] nums) {
+    public void sortColors1(int[] nums) {
         int s = 0;
         int m = 0;
-        int e = nums.length -1;
+        int e = nums.length - 1;
         while (m <= e) {
             if (nums[m] == 0) {
                 swap(nums, s, m);
@@ -33,6 +33,41 @@ public class SortColors {
         int tmp = nums[a];
         nums[a] = nums[b];
         nums[b] = tmp;
+    }
+
+    /*
+     * Time Complexity O(n)
+     * Space Complexity O(1)
+     *
+     * Counting sort
+     *
+     * 2-pass
+     * */
+
+    public void sortColors2(int[] nums) {
+        int zero = 0;
+        int one = 0;
+        int two = 0;
+
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] == 0) {
+                zero++;
+            } else if (nums[i] == 1) {
+                one++;
+            } else {
+                two++;
+            }
+        }
+
+        for (int i = 0; i < nums.length; i++) {
+            if (i < zero) {
+                nums[i] = 0;
+            } else if (i < zero + one) {
+                nums[i] = 1;
+            } else {
+                nums[i] = 2;
+            }
+        }
     }
 
 }
