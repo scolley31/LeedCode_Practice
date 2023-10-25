11646ef58aac11bffa24de8e642f732d75ff0f5f
scolley <scolley.huang@kkday.com>
2023-10-26 00:31:32 +0800

    704. Binary Search

--- Binary Search/704. Binary Search/BinarySearch.java
+++ Binary Search/704. Binary Search/BinarySearch.java
@@ -7,21 +7,38 @@ package org.example;
  * */
 
 public class BinarySearch {
-    class Solution {
-        public int search(int[] nums, int target) {
-            int left = 0;
-            int right = nums.length - 1;
-            while (left <= right) {
-                int mid = left + (right - left)/2;
-                if (nums[mid] > target) {
-                    right = mid - 1;
-                } else if (nums[mid] < target) {
-                    left = mid + 1;
-                } else {
-                    return  mid;
-                }
-            }
-            return -1;
+
+    // recursive
+    public int search2(int[] nums, int target) {
+        return binarySearch(nums, target, 0, nums.length-1);
+    }
+
+    private int binarySearch(int[] nums, int target, int l, int r) {
+        if (l > r) return -1;
+        int mid = l + (r - l)/2;
+        if (nums[mid] == target) {
+            return mid;
+        } else if (nums[mid] > target) {
+            return binarySearch(nums, target, l, mid - 1);
+        } else {
+            return binarySearch(nums, target, mid + 1, r);
         }
+    }
+
+    // iterative
+    public int search1(int[] nums, int target) {
+        int left = 0;
+        int right = nums.length - 1;
+        while (left <= right) {
+            int mid = left + (right - left)/2;
+            if (nums[mid] > target) {
+                right = mid - 1;
+            } else if (nums[mid] < target) {
+                left = mid + 1;
+            } else {
+                return  mid;
+            }
+        }
+        return -1;
     }
 }
